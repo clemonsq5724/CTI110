f9bcab401a443eb399d651a07d0f21aced3550e6
clemonsq5724 <143745607+clemonsq5724@users.noreply.github.com>
2023-10-12 14:17:29 -0400

    Add files via upload

--- P4LAB1_Clemons.py
+++ P4LAB1_Clemons.py
@@ -1,31 +1,60 @@
-# CTI 110
-# P4LAB1 - Part B
-# Text "graphics"
-# Clemons
-# 10/5/23
+"""
+This is the Template Repl for Python with Turtle.
 
-# Draw a rectangle
-CHAR = "😈"
-#print(CHAR)
+Python with Turtle lets you make graphics easily in Python.
 
-# Part 1: Draw a horizontal line
-WIDTH = int(input("How wide is the rectangle?"))
-HEIGHT = int(input("How tall is it?"))
+Check out the official docs here: https://docs.python.org/3/library/turtle.html
+"""
 
-                   
-print("Printing", WIDTH, "columns")
-for column in range(WIDTH):
-    #don't go to newline
-    print(CHAR, end="")
-print()
+import turtle
+import random
 
-# Part 2: Verticle line
-for row in range(HEIGHT):
-    print(CHAR) # we want the newline
-# Part 3: Draw the rectangle
+t = turtle.Turtle()
+t.pensize(3)
 
-for row in range(HEIGHT):
-    for column in range(WIDTH):
-        print(CHAR, end="")
-    print() # end the line
+# Set our variables
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+PEN_COLOR = random.choice(COLORS)
+SIDES = 3
+LENGTH = 100
+FILL = True
+REPEAT = 10
+for time in range(REPEAT):
+  X = random.randrange(-200, 200)
+  Y = random.randrange(-200, 200)
+  t.penup()
+  t.goto(X, Y)
+  t.pendown()
+  
+  FILL_COLOR = random.choice(COLORS)
     
+  # optional: let user pick
+  SIDES = random.randrange(3, 12)
+  LENGTH = random.randrange(10, 50)
+  ANGLE = 360 / SIDES
+    
+    
+  if FILL == True:
+      
+    t.begin_fill()
+    t.fillcolor(FILL_COLOR)
+    
+    
+    
+    
+  # draw a simple shape
+  t.pencolor(PEN_COLOR)
+  for side in range(SIDES):
+    t.forward(LENGTH)
+    t.right(ANGLE)
+    
+    
+  if FILL == True:
+     t.end_fill()
+  
+# keep window open(at end)
+win = turtle.Screen()
+win.exitonclick()
+
